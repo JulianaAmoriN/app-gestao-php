25e836055736f91635f715d3040fa69667173fc1
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-08 19:25:11 -0300

    estudando @if do blade

--- app_gestao_php/resources/views/app/fornecedor/index.blade.php
+++ app_gestao_php/resources/views/app/fornecedor/index.blade.php
@@ -6,9 +6,28 @@ teste teste
 {{-- Comentário no código --}}
 
 @php
-echo 'oi';
+    echo 'oi';
     // comentário de uma linha 
     /*
         comentário com mais de uma linha 
     */
+
+    /*if(){
+
+    }else if(){
+
+    }else{
+        
+    }*/
+
 @endphp
+
+{{-- @dd($fornecedores); --}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores</h3>
+@else
+    <h3>Não existem fornecedores</h3>
+@endif
