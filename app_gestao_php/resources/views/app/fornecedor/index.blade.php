04e3635854c7deab26071bdaf88fbaad0ff5aeb2
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-08 19:05:38 -0300

    incluindo comentarios e blocos puros de PHP

---
+++ app_gestao_php/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,14 @@
+teste teste 
+
+{{ 'vai de string'}}
+<?= 'ambas as tags funcionam iguais' ?>
+
+{{-- Comentário no código --}}
+
+@php
+echo 'oi';
+    // comentário de uma linha 
+    /*
+        comentário com mais de uma linha 
+    */
+@endphp
