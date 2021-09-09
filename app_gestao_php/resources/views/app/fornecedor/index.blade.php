bb98059e177a9b130ec7d4f657960a2d51af95ea
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-09 10:04:58 -0300

    estudando loops

--- app_gestao_php/resources/views/app/fornecedor/index.blade.php
+++ app_gestao_php/resources/views/app/fornecedor/index.blade.php
@@ -34,10 +34,6 @@ teste teste
 
 <br>
 {{-- @unless executa se o retorno for false, similar ao If(!<condição>){} --}}
-Fornecedor: {{$fornecedores[0]['nome']}}
-Status: {{$fornecedores[0]['status']}}
-CNPJ: {{$fornecedores[0]['cnpj']}}
-
 <br>
 @if(!($fornecedores[0]['status'] == 'n'))
     Fornecedor está ativo.
@@ -66,5 +62,78 @@ CNPJ: {{$fornecedores[0]['cnpj']}}
 @empty($oi)
 $oi foi declarado mas não recebeu nenhum valor 
 @endempty
+
+<br>
+@isset($fornecedores)
+    @for($i=0; isset($fornecedores[$i]); $i++)
+        <br>
+        Fornecedor: {{$fornecedores[$i]['nome']}}
+        <br>
+         Status: {{$fornecedores[$i]['status']}}
+        <br>
+        CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'não existe'}}
+        {{-- se a varável testada não estiver definida (retorno do isset) || variável testada possuir valor null --}}
+        <br>
+        @switch($fornecedores[$i]['ddd'])
+            @case('11')
+                São Paulo -SP
+                @break
+            @case('32')
+                Juiz de Fora -MG
+                @break
+            @default
+                Estado não registrado
+        @endswitch
+        <hr>
+    @endfor
+@endisset
+
+<br>
+@for($i=0; $i<=10; $i++)
+    {{ $i }} <br>
+@endfor
+
+@isset($fornecedores)
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
+        <br>
+        Fornecedor: {{$fornecedores[$i]['nome']}}
+        <br>
+        Status: {{$fornecedores[$i]['status']}}
+        <br>
+        CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'não existe'}}
+        <hr>
+        @php  $i++ @endphp
+    @endwhile
+@endisset
+
+@isset($fornecedores)
+    {{-- forEach gera um novo array com base no inicial, modificações feitas na variálvel de copia não altera valor do original--}}
+    @foreach($fornecedores as $indice => $fornecedor)
+        <br>
+        Fornecedor: {{$fornecedor['nome']}}
+        <br>
+        Status: {{$fornecedor['status']}}
+        <br>
+        CNPJ: {{$fornecedor['cnpj'] ?? 'não existe'}}
+        <hr>
+    @endforeach
+@endisset
+
+@php $teste= [] @endphp
+@isset($teste)
+    @forelse($teste as $indice => $fornecedor)
+        <br>
+        Fornecedor: {{$fornecedor['nome']}}
+        <br>
+        Status: {{$fornecedor['status']}}
+        <br>
+        CNPJ: {{$fornecedor['cnpj'] ?? 'não existe'}}
+        <hr>
+    @empty
+        Não existem fornecededores cadastrados
+    @endforelse
+@endisset
+
 
 
