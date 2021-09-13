ff267ef7e297811776986c1f94eb62a54acdbc8b
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-13 12:23:42 -0300

    criando migrate com relaçao de fk 1:n

---
+++ app_gestao_php/database/migrations/2021_09_13_150145_create_unidades_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade', 5);
+            $table->string('descricao', 30);
+            $table->timestamps();
+        });
+
+        //adicionar o relacionamento na tabela de produtos e produtos produtos_detalhes
+        Schema::table('produtos', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+
+        Schema::table('produtos_detalhes', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //remover o relacionamento das tabelas, primeiro a fk e depois a coluna unidade_id
+        Schema::table('produtos_detalhes', function(Blueprint $table){
+            $table->dropForeign('produtos_detalhes_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+        Schema::table('produtos', function(Blueprint $table){
+            $table->dropForeign('produtos_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+        Schema::dropIfExists('unidades');
+    }
+}
