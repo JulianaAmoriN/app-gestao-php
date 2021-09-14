a19a7cdef0998627bb687e174e41587d1f7f097d
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-14 10:00:44 -0300

    colocando o nome correto da tabela

--- app_gestao_php/app/Fornecedor.php
+++ app_gestao_php/app/Fornecedor.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //salvar o nome correto da tabela;
+    protected $table = 'fornecedores';
+    protected $fillable = ['name', 'site', 'uf', 'email'];
 }
