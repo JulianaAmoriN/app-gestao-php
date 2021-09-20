e532edc9d0e9313ec495052fab386567e70ac6ce
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-20 14:44:45 -0300

    criando classes semeadoras para popular as tabelas usando a lib faker

--- app_gestao_php/database/seeds/SiteContatoSeeder.php
+++ app_gestao_php/database/seeds/SiteContatoSeeder.php
@@ -12,20 +12,22 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        $contato = new SiteContato();
-        $contato->name = 'Ana';
-        $contato->telefone = '(11) 94998-7896';
-        $contato->email = 'ana@gmail.com';
-        $contato->motivo_de_contato = 2;
-        $contato->mensagem = 'adorei o app';
-        $contato->save();
+        // $contato = new SiteContato();
+        // $contato->name = 'Ana';
+        // $contato->telefone = '(11) 94998-7896';
+        // $contato->email = 'ana@gmail.com';
+        // $contato->motivo_de_contato = 2;
+        // $contato->mensagem = 'adorei o app';
+        // $contato->save();
 
-        DB::table('site_contatos')->insert([
-            'name' => 'Gael',
-            'telefone' => '(13) 48516-6345',
-            'email' => 'gael@gmail.com',
-            'motivo_de_contato' => 1,
-            'mensagem' => 'tem opção mobile?'
-         ]);
+        // DB::table('site_contatos')->insert([
+        //     'name' => 'Gael',
+        //     'telefone' => '(13) 48516-6345',
+        //     'email' => 'gael@gmail.com',
+        //     'motivo_de_contato' => 1,
+        //     'mensagem' => 'tem opção mobile?'
+        // ]);
+
+        factory(SiteContato::class, 10)-> create();
     }
 }
