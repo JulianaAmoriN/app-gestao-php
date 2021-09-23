5ddabd8868b96dc1a6f8d6969bb7898815603986
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-23 18:10:49 -0300

    adicionando o array na pagina principal pra funcionar

--- app_gestao_php/app/Http/Controllers/PrincipalController.php
+++ app_gestao_php/app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,12 @@ class PrincipalController extends Controller
 {
     public function principal()
     {
-        return view('site.principal',['titulo'=>'Home']);
+        $motivos_de_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclmação'
+        ];
+
+        return view('site.principal',['titulo'=>'Home', 'motivos_de_contatos'=> $motivos_de_contatos]);
     }
 }
--- app_gestao_php/resources/views/site/principal.blade.php
+++ app_gestao_php/resources/views/site/principal.blade.php
@@ -28,7 +28,7 @@
         <div class="contato">
             <h1>Contato</h1>
             <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
-            @component('site.layouts._components.form-contato', ['classe'=>'borda-branca'])@endcomponent
+            @component('site.layouts._components.form-contato', ['classe'=>'borda-branca',  'motivos_de_contatos'=> $motivos_de_contatos])@endcomponent
         </div>
     </div>
 </div>
