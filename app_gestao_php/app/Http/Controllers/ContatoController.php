d7c22bced786a5bbf3bd5ece035600ac5c624e67
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-23 14:13:30 -0300

    salvando dados do request no banco

--- app_gestao_php/app/Http/Controllers/ContatoController.php
+++ app_gestao_php/app/Http/Controllers/ContatoController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        var_dump($_POST);
+        // print_r($request->all());
+        // echo  $request->input('name');
+
+        $contato = new SiteContato();
+        // $contato->name = $request->input('name');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_de_contato = $request->input('motivo_de_contato');
+        // $contato->mensagem = $request->input('mensagem');
+
+        $contato->create($request->all());
+        print_r($contato->getAttributes());
+
+        //$contato->save();
+        
         return view('site.contato', ['titulo'=>'Contato']);
     }
 }
